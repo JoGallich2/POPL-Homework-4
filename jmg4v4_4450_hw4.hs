ff710dc875f9922aa9a7a35094936c143dfdc4f1
Joey Gallichio <158005169+JoGallich2@users.noreply.github.com>
2024-10-28 20:16:49 +0000

    Initial editing with moving instructions to the README.md

--- jmg4v4_4450_hw4.hs
+++ jmg4v4_4450_hw4.hs
@@ -1,72 +1,8 @@
 {-
 
-      student:
-      date:
-      pawprint:
-
-      HOMEWORK INSTRUCTIONS
-
-      1. READ THE DIRECTIONS CAREFULLY.
-
-      2a. Change "yourPawprint" in the file name to be your
-            actual pawprint
-
-      2b. Load the file into ghci on your local machine
-            or online at repl.it using
-              ":l <yourfilename>.hs"
-
-      3. Work your way down the document, filling in
-            code or type signatures where specified.
-
-            You MAY NOT alter type signatures if already
-            present. You MAY NOT change the names of the
-            functions specified, these will be used for
-            automated testing and must achieve the behavior
-            specified.
-
-            You MAY write helper functions to assist in
-            the logic of your functions.
-
-      4. There are 4 parts to this assignment. As they progress, they will
-              progress in difficulty, while building on eachother. It is
-              recommended that you use the hints in the comments, along
-              with the code from lecture to solve the problems given.
-
-           5A. Part I   - Types     (20 pts)
-           5B. Part II  - Lists     (20 pts)
-           5C. Part III - Functions (10 pts)
-                        - Total     (50 pts)
-           5D. Part IV  - Bonus     (5  pts)
-
-      EVALUATION CRITERIA
-
-      1. You will be evaluated using automated tests
-        that compare your answers to randomly generated
-        values that pass tests I write.
-
-      2. Your code must type check. That is, no errors
-        can be thrown when I load your file into ghc.
-        It is better to define a function as undefined
-        than to let it ruin the compiliation.
-
-      3. You will receive +1 point for an attempt that does
-          not break the compiler, but is not correct.
-         You will receive other partial credit if specified.
-         You will receive 0 points for an undefined function.
-         You will receive +1 point for a correct type signature
-          even if you leave the function undefined. (If you were
-          prompted to fill in the type signature)
-
-      4. The point values of each function are specified directly
-            above the functions themselves.
-
-      5. Cheating is not tolerated. Refrain from copying code from eachother,
-            but feel free to use the internet as a resource. If you feel the
-            need to directly copy someone's code, please cite it in comments
-            using "--" to start a line comment.
-
-
-
+      student: Joey Gallichio
+      date: 10/28/2024
+      pawprint: jmg4v4
 
 -}
 
